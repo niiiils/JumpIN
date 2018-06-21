dd1de7001f246c26a08b4d6f91ef44c39bddeb0a
Nils <38754158+niiiils@users.noreply.github.com>
2018-06-21 10:39:40 +0200

    menu buttons added

--- JumpIN/JumpIN/MainPage.xaml.cs
+++ JumpIN/JumpIN/MainPage.xaml.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace JumpIN
 {
-	public partial class MainPage : ContentPage
-	{
-        private DayOfWeek now;
+    public partial class MainPage : ContentPage
+    {
+        private DayOfWeek today;
+        private String currentYear = DateTime.Now.Year.ToString();
 
         public MainPage()
         {
             InitializeComponent();
-            BoxView box = this.FindByName<BoxView>("box1");
-            now = DateTime.Now.DayOfWeek;
-            String currently = Convert.ToString(now);
+            //BoxView box = this.FindByName<BoxView>("box1");
+
+            today = DateTime.Now.DayOfWeek;
+            String currently = Convert.ToString(today);
             Label currentlyLabel = this.FindByName<Label>("datetimeLabel");
             currentlyLabel.Text = currently;
+            Label currentYearLabel = this.FindByName<Label>("year");
+            currentYearLabel.Text = currentYear;
+
+            
+            
         }
-	}
+
+        void OnTapped(object sender, EventArgs e)
+        {
+            Console.WriteLine("du larry");
+        }
+    }
 }
